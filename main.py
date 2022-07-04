4066d9767b1a87e4d95e928f849b7cbdf93363d0
ps <dhjkjdfk/@.1>
2022-07-04 20:41:24 +0300

    проверка на двое раввных чисел

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ if n==v==g:
     print(3)
 elif n!=v!=g:
     print(0)
+elif g==n and g!=v and n!=v:
+    print(2)
+elif g==v and g!=n and v!=n:
+    print(2)
+elif v==n and v!=g and n!=g:
+    print(2)
